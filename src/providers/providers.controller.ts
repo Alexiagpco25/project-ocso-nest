4d84bf0e89470770b640fa7be36472ce52b39b57
Alexiagpco25 <alexiagpco@gmail.com>
2025-03-07 15:47:38 -0600

    User relations to manager and employees

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/auth/entities/user.entity';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @UseGuards(AuthGuard)
 @Controller('providers')
@@ -19,8 +20,7 @@ export class ProvidersController {
   }
 @UseGuards(AuthGuard)
 
-  @Roles(["Admin"])
-  @UseGuards(RolesGuard)
+  @Auth("Employee")
   @Get()
   findAll(@UserData() user: User) {
     if(user.userRoles.includes("Employee")) throw new UnauthorizedException("No estas autorizado,solo admins y managers")
